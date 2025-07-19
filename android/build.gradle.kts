31740d7a0e5dcfd2ef85f72ee30be7886dd9ee17
Prathamesh Rasam <45089785+prasam25@users.noreply.github.com>
2025-07-19 23:12:30 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -36,5 +36,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-    implementation("co.epsilondelta:orion-flutter:1.0.0")
+    implementation("co.epsilondelta:orion-flutter:1.0.1")
 }
