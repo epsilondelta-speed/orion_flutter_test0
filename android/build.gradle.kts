a3d537469102245331fd8c50f1e3d5c321a2c57e
Prathamesh Rasam <45089785+prasam25@users.noreply.github.com>
2025-07-21 16:58:00 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -36,5 +36,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
-    implementation("co.epsilondelta:orion-flutter:1.0.1")
+    implementation("co.epsilondelta:orion-flutter:1.0.1"){
+        exclude group: 'io.flutter', module: 'flutter_embedding_release'
+    }
+    
 }
